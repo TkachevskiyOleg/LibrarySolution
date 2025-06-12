570b87846aaa8305482b222fb0fbe851ec06c4f0
Misteri <136891190+TkachevskiyOleg@users.noreply.github.com>
2025-06-12 13:41:28 +0300

    фікс

---
+++ Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorFullName { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public int Year { get; set; }
+    }
+}
