7d650f82189879057a772ce6bf362fbdac7b3f68
Misteri <136891190+TkachevskiyOleg@users.noreply.github.com>
2025-06-13 15:43:08 +0300

    fix

--- Author.cs
+++ Author.cs
@@ -2,5 +2,7 @@
 {
     public class Author
     {
+        public int Id { get; set; }                  
+        public string FullName { get; set; } = string.Empty;  
     }
 }
--- Book.cs
+++ Book.cs
@@ -3,10 +3,10 @@
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string AuthorFullName { get; set; }
-        public string Genre { get; set; }
-        public string Publisher { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string AuthorFullName { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Publisher { get; set; } = string.Empty;
         public int Year { get; set; }
     }
 }
